470142b890628d0515e15aced847dae3bbf03908
8Balu8 <barzagliluca@gmail.com>
2022-02-17 16:17:45 +0100

    add styles to index.blade

--- resources/views/comics/index.blade.php
+++ resources/views/comics/index.blade.php
@@ -5,14 +5,17 @@
 @endsection
 
 @section('content')
-    <h1>Lista Comics</h1>
-
-    @foreach ($comics as $comic)
-        <div>
-            <a href="{{route('comics.show', ['comic' => $comic->id])}}">
-                <h2>{{$comic->title}}</h2>
-            </a>
-            <span>{{$comic->type}}</span>
-        </div>  
-    @endforeach
+    <h1 class="text-center">Lista Comics</h1>
+    <div class="container">
+        @foreach ($comics as $comic)
+            <div class="list-group">
+                <a href="{{route('comics.show', ['comic' => $comic->id])}}" class="list-group-item list-group-item-action">
+                    <div class="d-flex w-100 justify-content-between">
+                    <h5 class="mb-1">{{$comic->title}}</h5>
+                    </div>
+                    <p class="mb-1">{{$comic->type}}</p>
+                </a>
+            </div>
+        @endforeach
+    </div>
 @endsection
